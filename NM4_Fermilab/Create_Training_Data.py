4b232128ba8e7dc45723b7e9ee8aa67c2dcd017e
das5pzq <das5pzq@virginia.edu>
2024-09-16 16:28:31 -0400

    Updates

--- NM4_Fermilab/Create_Training_Data.py
+++ NM4_Fermilab/Create_Training_Data.py
@@ -7,7 +7,6 @@ import numpy as np
 import pandas as pd
 from Lineshape import *
 from scipy.integrate import quad
-from math import log, floor
 from tqdm import tqdm
 import os 
 import sys
@@ -19,35 +18,17 @@ SNR_arr = []
 
 for i in tqdm(range(1000)):  
 
-    # U = 2.4283e1 + np.random.uniform(-0.01, 0.01)
-    # Cknob = .0647 + np.random.uniform(-0.005, 0.005)
-    # cable = 22/2
-    # eta = 1.04e-2 + np.random.uniform(-0.001, 0.001)
-    # phi = 6.1319 + np.random.uniform(-0.1, 0.1)
-    # Cstray = 10**(-15)
-    # shift = -2.0464e-2 + np.random.uniform(-0.001, 0.001)
-    # sig = 0.01 + np.random.uniform(-0.009, 0.1)              # Gaussian width variation
-    # gam = 0.01 + np.random.uniform(-0.009, 0.1)              # Lorentzian width variation
-    # amp = .002 + np.random.uniform(-0.0019, 0.005)
-    # center = 213 + np.random.uniform(-.3, .3)
-
-    U = 2.4283e1 
-    Cknob = .0647 
-    cable = 22/2
-    eta = 1.04e-2 
-    phi = 6.1319
-    Cstray = 10**(-15)
-    shift = -2.0464e-2
-    sig = 0.01            
-    gam = 0.01         
-    amp = .002 + np.random.uniform(-0.0019, 0.005)
-    center = 213 
+    sig = 0.01 + np.random.uniform(-0.009, 0.1)       
+    gam = 0.01 + np.random.uniform(-0.009, 0.1)         
+    amp = .01 + np.random.uniform(-0.0019, 0.1)
+    center = 213 + np.random.uniform(-.3, .3)
 
     x, lower_bound, upper_bound = FrequencyBound(212.6)
 
     signal = Voigt(x, amp, sig, gam, center)
 
-    baseline = Baseline(x, U, Cknob, eta, cable, Cstray, phi, shift)
+    # baseline = Baseline(x, U, Cknob, eta, cable, Cstray, phi, shift)
+    baseline = -0.001717*x **2 + 0.732*x - 77.99
 
     combined_signal = signal + baseline
 
