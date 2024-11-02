4e47d1b939f84a0761cb049303472dd7b1cfc266
das5pzq <das5pzq@virginia>
2024-11-02 11:36:18 -0400

    Updates

--- NM4_Fermilab/Testing.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r"NM4_Fermilab\Model Performance\v9\Results.csv")
-lowest, highest = df['Area_True'].min(), df['Area_True'].max()  # Replace 'your_column_name' with your actual column name
-print(lowest, highest)
