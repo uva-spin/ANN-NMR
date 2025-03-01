7bf63e8444ccb2d582d410addca0f2c5243849bc
das5pzq <das5pzq@virginia.edu>
2025-02-28 19:08:03 -0500

    ...

--- NM4_Fermilab/Training_Simple.py
+++ NM4_Fermilab/Training_Simple.py
@@ -38,33 +38,35 @@ if physical_devices:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
     print(f"Using GPU: {physical_devices[0]}")
 
-# File paths and versioning
-data_path = find_file("Deuteron_Low_No_Noise_500K.csv")  
-version = 'Deuteron_Low_Noise_HighPrecision_V3'  
+
+data_path = find_file("Deuteron_2_100_No_Noise_500K.csv")  
+data_path_lower = find_file("Deuteron_0_2_No_Noise_500K.csv")  
+data_path_lowest = find_file("Deuteron_Low_No_Noise_500K.csv")  
+version = 'Deuteron_All_No_Noise_XGBoost_V1'  
 performance_dir = f"Model Performance/{version}"  
 model_dir = f"Models/{version}"  
 os.makedirs(performance_dir, exist_ok=True)
 os.makedirs(model_dir, exist_ok=True)
-# Load and prepare data
+
 print("Loading and preparing data...")
 data = pd.read_csv(data_path)
+data_lower = pd.read_csv(data_path_lower)
+data_lowest = pd.read_csv(data_path_lowest)
 
-# Scale the input features
+data = pd.concat([data, data_lower, data_lowest], ignore_index=True)
+
 feature_scaler = MinMaxScaler()
 X = data.drop(columns=["P", 'SNR']).astype('float64').values
 X_scaled = feature_scaler.fit_transform(X)
 
-# Do not scale the target values
 y = data["P"].astype('float64')
 
-# Split data
 print("Splitting data...")
 train_data, temp_data = train_test_split(data, test_size=0.3, random_state=42)
 val_data, test_data = train_test_split(temp_data, test_size=1/3, random_state=42)
 
-# Define the target value and range
 target_value = 0.0005
-range_margin = 0.0005  # Define a margin around the target value
+range_margin = 0.00005  
 multiplication_factor = 5  # Define how many times to duplicate the augmented data
 
 # Filter the training data for values around 0.05%
@@ -75,7 +77,6 @@ augmented_data = train_data[mask]
 augmented_data = pd.concat([augmented_data] * multiplication_factor, ignore_index=True)
 train_data = pd.concat([train_data, augmented_data], ignore_index=True)
 
-# Prepare feature matrices
 X_train = train_data.drop(columns=["P", 'SNR']).astype('float64').values
 y_train = train_data["P"].astype('float64')
 X_val = val_data.drop(columns=["P", 'SNR']).astype('float64').values
@@ -83,36 +84,34 @@ y_val = val_data["P"].astype('float64')
 X_test = test_data.drop(columns=["P", 'SNR']).astype('float64').values
 y_test = test_data["P"].astype('float64')
 
-# Scale the training, validation, and test sets for features only
-X_train_scaled = feature_scaler.fit_transform(X_train)  # Fit and transform on training data
-X_val_scaled = feature_scaler.transform(X_val)  # Transform validation data
-X_test_scaled = feature_scaler.transform(X_test)  # Transform test data
+X_train_scaled = feature_scaler.fit_transform(X_train)  
+X_val_scaled = feature_scaler.transform(X_val)
+X_test_scaled = feature_scaler.transform(X_test)  
 
 # Free up memory
 del data, train_data, val_data, test_data
 import gc
 gc.collect()
 
-# Create and train the XGBoost model with the custom loss function
 xgb_model = XGBRegressor(
-    n_estimators=5000,  # Reduced number of boosting stages for simplicity
-    learning_rate=0.01,  # Increased learning rate for faster convergence
-    max_depth=7,  # Reduced maximum depth to simplify the model
+    n_estimators=5000, 
+    learning_rate=0.01, 
+    max_depth=4,  
     random_state=42,
-    eval_metric='rmse',  # Metric to evaluate
-    objective='reg:squarederror',  # Objective function for regression
-    tree_method='gpu_hist',  # Use GPU for training
-    early_stopping_rounds=50,  # Reduced rounds for early stopping
-    alpha=0.1,  # Removed L1 regularization term
-    lambda_=0.1,  # Removed L2 regularization term
+    eval_metric='rmse',  
+    objective='reg:squarederror',  
+    tree_method='gpu_hist', 
+    early_stopping_rounds=50,  
+    alpha=0.1,  # L1 regularization term
+    lambda_=2,  # L2 regularization term
     subsample=0.8  # Use 80% of the data for each boosting round
 )
 
-# Fit the model with the custom loss function
 xgb_model.fit(X_train_scaled, y_train,
-              eval_set=[(X_val_scaled, y_val)],  # Specify the validation set
+              eval_set=[(X_val_scaled, y_val)],  
               verbose=True)
-# Make predictions
+
+
 y_val_pred = xgb_model.predict(X_val_scaled)
 y_test_pred = xgb_model.predict(X_test_scaled)
 
@@ -121,7 +120,6 @@ y_test *= 100
 y_val_pred *= 100
 y_test_pred *= 100
 
-# Evaluate the model
 val_mae = mean_absolute_error(y_val, y_val_pred)
 val_rmse = mean_squared_error(y_val, y_val_pred, squared=False)
 
@@ -133,14 +131,12 @@ print(f"Validation RMSE: {val_rmse:.6f}")
 print(f"Test MAE: {test_mae:.6f}")
 print(f"Test RMSE: {test_rmse:.6f}")
 
-# Calculate relative percent error
 relative_percent_error = np.abs((y_test - y_test_pred) / y_test) * 100
 
-# Create a directory for saving model performance plots
-output_dir = 'Model Performance/XGBoost_PerformanceV2'
-os.makedirs(output_dir, exist_ok=True)
+# Filter out outliers where RPE is greater than 10%
+mask_outliers = relative_percent_error <= 10  # Create a mask for RPE less than or equal to 10%
+relative_percent_error_filtered = relative_percent_error[mask_outliers]
 
-# Plotting the training and validation losses
 plt.figure(figsize=(10, 6))
 plt.plot(np.arange(1, len(xgb_model.evals_result()['validation_0']['rmse']) + 1), 
          xgb_model.evals_result()['validation_0']['rmse'], label='Validation Loss', color='orange')
@@ -149,27 +145,24 @@ plt.xlabel('Number of Iterations', fontsize=14)
 plt.ylabel('Root Mean Squared Error', fontsize=14)
 plt.legend()
 plt.grid()
-# Save the figure
-plt.savefig(os.path.join(output_dir, 'validation_loss.png'))
-plt.close()  # Close the figure to free up memory
+plt.savefig(os.path.join(performance_dir, 'validation_loss.png'))
+plt.close()
 
 # Histogram of relative percent error
 plt.figure(figsize=(10, 6))
-plt.hist(relative_percent_error, bins=30, color='skyblue', edgecolor='black')
+plt.hist(relative_percent_error, bins=200, color='skyblue', edgecolor='black')
 plt.title('Histogram of Relative Percent Error', fontsize=16)
 plt.xlabel('Relative Percent Error (%)', fontsize=14)
 plt.ylabel('Frequency', fontsize=14)
 plt.grid()
-# Save the figure
-plt.savefig(os.path.join(output_dir, 'relative_percent_error_histogram.png'))
-plt.close()  # Close the figure to free up memory
+plt.savefig(os.path.join(performance_dir, 'relative_percent_error_histogram.png'))
+plt.close()  
 
-# Create a figure with two subplots
 plt.figure(figsize=(12, 6))
 
 # First subplot: Original scatter plot
 plt.subplot(1, 2, 1)
-plt.scatter(y_test, relative_percent_error, alpha=0.5)
+plt.scatter(y_test, relative_percent_error_filtered, alpha=0.5)
 plt.title('Relative Percent Error as a Function of Polarization', fontsize=16)
 plt.xlabel('Polarization', fontsize=14)
 plt.ylabel('Relative Percent Error (%)', fontsize=14)
@@ -178,12 +171,11 @@ plt.grid()
 # Second subplot: Focused scatter plot for polarizations < 0.06%
 plt.subplot(1, 2, 2)
 mask = y_test < 0.06  # Create a mask for polarizations less than 0.06%
-plt.scatter(y_test[mask], relative_percent_error[mask], alpha=0.5, color='orange')
+plt.scatter(y_test[mask], relative_percent_error_filtered[mask], alpha=0.5, color='orange')
 plt.title('Relative Percent Error for Polarization < 0.06%', fontsize=16)
 plt.xlabel('Polarization', fontsize=14)
 plt.ylabel('Relative Percent Error (%)', fontsize=14)
 plt.grid()
 
-# Save the figure
-plt.savefig(os.path.join(output_dir, 'relative_percent_error_vs_polarization_combined.png'))
-plt.close()  # Close the figure to free up memory
+plt.savefig(os.path.join(performance_dir, 'relative_percent_error_vs_polarization_combined.png'))
+plt.close()  
